b607c0dfbe7825f789ca6c72cea173c9b223f11e
Marcin Kozub <kozubmarcin10@gmail.com>
2023-01-13 18:17:56 +0100

    Add mqtt sensors scripts (#1)
    Co-authored-by: Marcin Kozub <407640@student.agh.edu.pl>

---
+++ Sensors/connectors.py
@@ -0,0 +1,103 @@
+import typing
+import uuid
+
+from paho.mqtt import client as mqtt_client
+
+
+class MQTTConnector:
+    # main_topic is actually a sensor_id (the main topic for sensor)
+    def __init__(self, broker: str, port: int, main_topic: str):
+        self.broker = broker
+        self.port = port
+        self.main_topic = main_topic
+
+        self.client_id = f"python-mqttt-{uuid.uuid1()}"
+        self.client = self.__connect_mqtt()
+        self.data: typing.Dict[str, str] = {}
+
+    def set_broker(self, broker: str):
+        self.broker = broker
+
+    def set_port(self, port: int):
+        self.port = port
+
+    def publish(self, topic: str, data: typing.Any):
+        # example of the actual topic: sensor-0/open
+        self.client.loop_start()
+        self.client.publish(self.__get_actual_topic(topic), data)
+
+    def subscribe(self, topic: str):
+        def on_message(client, userdata, msg):
+            self.data[topic] = msg.payload.decode("utf-8")
+            print(f"Received `{self.data[topic]}` of key from `{msg.topic}` topic")
+
+        self.data[topic] = "0"
+        self.client.loop_start()
+        actual_topic = self.__get_actual_topic(topic)
+        self.client.subscribe(actual_topic)
+        self.client.on_message = on_message
+
+    def unsubscribe(self, topic: str):
+        self.client.unsubscribe(self.__get_actual_topic(topic))
+
+    def __connect_mqtt(self) -> mqtt_client:
+        def on_connect(client_id: mqtt_client, userdata, flags, rc: int):
+            if rc == 0:
+                print("Connected to MQTT Broker!")
+            else:
+                print("Failed to connect, return code %d\n", rc)
+
+        client = mqtt_client.Client(client_id=self.client_id)
+        client.on_connect = on_connect
+
+        try:
+            client.connect(self.broker, self.port)
+        except ConnectionRefusedError:
+            print("Client was not able to connect to broker")
+
+        return client
+
+    def __get_actual_topic(self, topic: str):
+        return self.main_topic + "/" + topic
+
+
+class SensorConnector:
+    subscribed_topics: typing.Dict[str, bool] = dict()
+
+    def __init__(self, broker: str, port: int, main_topic: str):
+        self.broker = broker
+        self.port = port
+        self.main_topic = main_topic
+        self.connectors = {"publisher": MQTTConnector(broker, port, main_topic)}
+
+    def send_data(self, topic: str, data: typing.Any):
+        self.connectors["publisher"].publish(topic, data)
+
+    def get_data(self, topic: str) -> str:
+        if (
+            topic not in self.subscribed_topics.keys()
+            or topic not in self.connectors.keys()
+        ):
+            self.subscribed_topics[topic] = False
+            self.connectors[topic] = MQTTConnector(
+                self.broker, self.port, self.main_topic
+            )
+            self.subscribe(topic)
+        elif self.subscribed_topics[topic] is False:
+            self.subscribe(topic)
+
+        return self.connectors[topic].data[topic]
+
+    def subscribe(self, topic: str):
+        self.connectors[topic].subscribe(topic)
+        self.subscribed_topics[topic] = True
+
+    def unsubscribe(self, topic: str):
+        self.connectors[topic].unsubscribe(topic)
+        self.subscribed_topics[topic] = False
+
+    def unsubscribe_all(self):
+        for topic, value in self.subscribed_topics.items():
+            if value:
+                print("Unsubscribed topic -> ", topic)
+                self.unsubscribe(topic)
--- main.py
+++ main.py
@@ -1,16 +1,80 @@
-# This is a sample Python script.
+import fnmatch
+import os
+import subprocess
+import sys
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from Sensors import sensor, sensor_scripts
+import signal
+
+NAME = "sensor-"
+COUNTER = 0
+
+BROKER = "127.0.0.1"
+PORT = 8080
+
+CHILDREN = []
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def signal_handler(signum, frame):
+    print("Killing all children!")
+    for child in CHILDREN:
+        print("pid: ", child.pid)
+        child.terminate()
+        time.sleep(1)
+        print("status: ", child.poll())
+
+    print("Exit the program in 3 seconds...")
+    time.sleep(3)
+    exit(0)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def create_subprocess(class_name: str):
+    global COUNTER
+    p = subprocess.Popen(
+        ["python3", file] + [BROKER, str(PORT), NAME, str(COUNTER), class_name, NAME],
+        env=process_env,
+    )
+    print("Sensor child's pid: ", p.pid, " | Sensor type: ", class_name)
+    CHILDREN.append(p)
+    COUNTER += 1
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def locate(pattern, root=os.curdir):
+    """
+    Locate dir matching supplied dirname pattern in and below
+    supplied root directory.
+    """
+    for path, dirs, files in os.walk(os.path.abspath(root)):
+        for dirname in fnmatch.filter(dirs, pattern):
+            return os.path.join(path, dirname)
+
+
+if __name__ == "__main__":
+    # handling all signals
+    for i in [x for x in dir(signal) if x.startswith("SIG")]:
+        try:
+            signum = getattr(signal, i)
+            if isinstance(signum, signal.Signals):
+                signal.signal(signum, signal_handler)
+        except (OSError, RuntimeError):  # OSError for Python3, RuntimeError for 2
+            print("Skipping {}".format(i))
+
+    time.sleep(1)
+
+    file = os.path.join(os.getcwd(), "sensor.py")
+    process_env = os.environ.copy()
+
+    create_subprocess(sensor_scripts.GasValveSensor.__name__)
+    create_subprocess(sensor_scripts.SmartPlug.__name__)
+    create_subprocess(sensor_scripts.Lock.__name__)
+    create_subprocess(sensor_scripts.GasDetector.__name__)
+    create_subprocess(sensor_scripts.Light.__name__)
+    create_subprocess(sensor_scripts.TemperatureSensor.__name__)
+    create_subprocess(sensor_scripts.HumidSensor.__name__)
+    create_subprocess(sensor_scripts.RollerShade.__name__)
+    create_subprocess(sensor_scripts.GarageDoor.__name__)
+    create_subprocess(sensor_scripts.RollerShade.__name__)
+
+    while True:
+        pass
